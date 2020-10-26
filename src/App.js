7bf00cdefeaab5036092ec189e9cb9c5901cce54
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-10-26 20:43:59 +0300

    Create a general layout

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Tic Tac Toe
+        </h1>
+        
       </header>
+      <main className="App-main">
+        Hello
+      </main>
+      <footer className="App-footer"> 
+        Made with love in Ankara, Turkey
+      </footer>
     </div>
   );
 }
