09058f995358d6dd26363c7c51cd402f69cec63e
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-10-27 23:25:22 +0300

    Add button to reset the game

--- src/components/game.js
+++ src/components/game.js
@@ -1,13 +1,21 @@
 const Game = () => {
     return (
         <div>
-            <button >New Game</button>
+            <button style={styledButton}>New Game</button>
         </div>
     )
 }
 
 const styledButton = {
-    
+    margin: '20px 0 0', 
+    color: 'white', 
+    background: 'rgb(44 93 191)', 
+    height: '45px',
+    width: '125px', 
+    border: 'none', 
+    borderRadius: '3px', 
+    boxShadow: '0 2px 15px black', 
+    fontSize: '17px'
 }
 
 export default Game
